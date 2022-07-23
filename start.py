a37facde002e251229792767bf03bf5780e831e2
Krista A <37197582+kristaoffical@users.noreply.github.com>
2022-07-22 17:15:09 -0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,14 @@
+#Who are you, young skywalker? - condenses str and capitlize user's name
+name = input("What's your name? ").strip().title()
+#Split user's name into first and last name
+#   first, last = name.split(" ")
+#Remove whitespace from str and capitalize user's name
+#   name = name.strip().title()
+#Capitalize user's name
+#   name = name.capitalize()
+#Capitalize user's name
+#   name = name.title()
+#Say hello to user
+print(f"hello, {name}")
+#Calling out variable name to print out the value of name
+#   print(name)
