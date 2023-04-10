229e173c0ee885bdb97efbd3daaca9e3fa3b9810
Krista A <37197582+kristaoffical@users.noreply.github.com>
2023-04-10 15:19:09 -0700

    Future Date
    Here is a Python script that calls to a future time based on the day of the week.

---
+++ datetime.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 14:37:30 2023
+
+@author: Krista
+"""
+
+import datetime
+
+def next_weekday(d, weekday):
+    days_ahead = weekday - d.weekday()
+    if days_ahead <= 0: #Counting current day
+        days_ahead += 7
+    return d + datetime.timedelta(days_ahead)
+
+from datetime import date
+today = date.today()
+
+#Change the number below to the future day of the week you want
+#0 = Monday, 1=Tuesday, 2=Wednesday...
+DayofWeek = next_weekday(today, 1)
+#Print the output
+print(DayofWeek.strftime("%m/%d/%Y"))
