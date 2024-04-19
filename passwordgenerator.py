a046c2254c4be858d65fff4ea0b6f00a009c0290
Krista A <andrewsmkrista@gmail.com>
2024-04-18 22:33:47 -0700

    Updated file
    Updated characters variable to include a proper punctuation variable.
    Removed the question asking to if the user wants to generate a password. Instead, went straight for a direct prompt for character length for a password.

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -4,11 +4,13 @@ Created on Wed Mar 20 20:41:15 2024
 
 @author: Krista
 """
-#modules
-import string
-import random
+# =============================================================================
+# #modules
+# Python allows imports to be on the same line and be seperated by a comma.
+# =============================================================================
+import string, random
 
-characters = list(string.ascii_letters + string.digits + "~`!@#$%^&*()_-+={[}]|:;<,>.?\/")
+characters = list(string.ascii_letters + string.digits + string.punctuation)
 
 def generate_password():
     #asking the user how long they want their password to be.
@@ -26,12 +28,19 @@ def generate_password():
     password =''.join(password)
     print(password)
     
-#Asking the user if they want to generate a password. 
-option = input("Do you want to generate a password?: ")
+generate_password()
 
-if option == 'Yes':
-    generate_password()
-elif option == 'No':
-    print('No password generated.')
-else:
-    print('Invalid input. Please restart the program and type "Yes" or "No". Thank you.')
+    
+#Asking the user if they want to generate a password. 
+# =============================================================================
+# option = input("Do you want to generate a password?: ")
+# 
+# #Added casefold method to if statement, so case-sensitivity won't be an issue. 
+# 
+# if option == 'Yes' or'Y'.casefold():
+#     generate_password()
+# elif option == 'No' or 'N'.casefold():
+#     print('No password generated.')
+# else:
+#     print('Invalid input. Please restart the program and type "Yes" or "No". Thank you.')
+# =============================================================================
